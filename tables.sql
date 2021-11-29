015928b1580af3f471902c018c93d06a60f5fd90
MariemElG <94700720+MariemElG@users.noreply.github.com>
2021-11-29 18:21:51 +0100

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,74 +1,110 @@
 
-CREATE TABLE IF NOT EXISTS Client (
-    email varchar(20) NOT NULL DEFAULT '',
-    motDePasse varchar(20) NOT NULL DEFAULT '',
-    nom varchar(20) NOT NULL DEFAULT '',
-    prenom varchar(20) NOT NULL DEFAULT '',
-    ville varchar(20) NOT NULL DEFAULT '',
-    addresse varchar(20) NOT NULL DEFAULT '',
-    numTelephone int(15) UNSIGNED DEFAULT '0'
-  
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `email` varchar(20) NOT NULL DEFAULT '',
+  `motDePasse` varchar(20) NOT NULL DEFAULT '',
+  `nom` varchar(20) NOT NULL DEFAULT '',
+  `prenom` varchar(20) NOT NULL DEFAULT '',
+  `ville` varchar(20) NOT NULL DEFAULT '',
+  `addresse` varchar(20) NOT NULL DEFAULT '',
+  `numTelephone` int(15) UNSIGNED DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* creation table produit*/
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS Produit (
-    idProduit tinyint(3) UNSIGNED DEFAULT '0',
-    nomProduit varchar(20) NOT NULL DEFAULT '', 
-    marque varchar(20) NOT NULL DEFAULT '', 
-    categorie varchar(20) NOT NULL DEFAULT '', 
-    descriptif varchar(500) NOT NULL DEFAULT '',
-    photo TEXT (100) NOT NULL DEFAULT '',
-    prix INT (6)  UNSIGNED DEFAULT '0',
-    stock varchar(20) NOT NULL DEFAULT ''
+--
+-- Structure de la table `commandes`
+--
+
+CREATE TABLE `commandes` (
+  `idCommande` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `date` date NOT NULL DEFAULT '0000-00-00',
+  `etat` varchar(20) NOT NULL DEFAULT '',
+  `email` varchar(20) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* creation table Commandes*/
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS Commandes (
-    idCommande int(10) UNSIGNED DEFAULT '0',
-    date date NOT NULL DEFAULT '0000-00-00',
-    etat varchar(20) NOT NULL DEFAULT ''
-   
+--
+-- Structure de la table `lignescommandes`
+--
+
+CREATE TABLE `lignescommandes` (
+  `idLigneCommande` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `idCommande` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `idProduit` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `quantite` int(5) UNSIGNED DEFAULT '0',
+  `montant` int(5) UNSIGNED DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* creation table LignesCommandes*/
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS Lignescommandes (
-    idLigneCommande int(10) UNSIGNED DEFAULT '0',
-   
-    quantite int(5) UNSIGNED DEFAULT '0',
-    montant int(5) UNSIGNED DEFAULT '0'
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `idProduit` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `nomProduit` varchar(20) NOT NULL DEFAULT '',
+  `marque` varchar(20) NOT NULL DEFAULT '',
+  `categorie` varchar(20) NOT NULL DEFAULT '',
+  `descriptif` varchar(500) NOT NULL DEFAULT '',
+  `photo` text NOT NULL,
+  `prix` int(6) UNSIGNED DEFAULT '0',
+  `stock` varchar(20) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* ajout des clés */
+--
+-- Index pour les tables déchargées
+--
 
-ALTER TABLE Client
-  ADD PRIMARY KEY (email);
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`email`);
 
-ALTER TABLE Produit
-  ADD PRIMARY KEY (idProduit);
+--
+-- Index pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`idCommande`),
+  ADD KEY `Commandes_Client` (`email`);
 
+--
+-- Index pour la table `lignescommandes`
+--
+ALTER TABLE `lignescommandes`
+  ADD PRIMARY KEY (`idLigneCommande`),
+  ADD KEY `idCommande` (`idCommande`),
+  ADD KEY `idProduit` (`idProduit`);
 
-ALTER TABLE Commandes
-  ADD PRIMARY KEY (idCommande),
-  ADD KEY email (email);
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`idProduit`);
 
-ALTER TABLE Lignescommandes
-    ADD PRIMARY KEY (idLigneCommande),
-    ADD KEY idCommande (idCommande),
-    ADD KEY idProduit (idProduit);
+--
+-- Contraintes pour les tables déchargées
+--
 
+--
+-- Contraintes pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD CONSTRAINT `Commandes_Client` FOREIGN KEY (`email`) REFERENCES `client` (`email`);
 
-ALTER TABLE Commandes
-  ADD CONSTRAINT Commandes_Client FOREIGN KEY (email) REFERENCES Client (email);
-
-
-ALTER TABLE Lignescommandes
-  ADD CONSTRAINT Lignescommandes_Commandes FOREIGN KEY (idCommande) REFERENCES Commandes (idCommande),
-  ADD CONSTRAINT Lignescommandes_Produit FOREIGN KEY (idProduit) REFERENCES Produit (idProduit);
-
+--
+-- Contraintes pour la table `lignescommandes`
+--
+ALTER TABLE `lignescommandes`
+  ADD CONSTRAINT `lignescommandes_ibfk_1` FOREIGN KEY (`idCommande`) REFERENCES `commandes` (`idCommande`),
+  ADD CONSTRAINT `lignescommandes_ibfk_2` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`idProduit`);
 COMMIT;
-
-
-
