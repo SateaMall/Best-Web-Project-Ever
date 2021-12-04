e8d260416030c66166a724e0524c6bd30974e62b
SateaMall <75503482+SateaMall@users.noreply.github.com>
2021-12-04 03:09:53 +0100

    Update index.php

--- WebSiteCode/index.php
+++ WebSiteCode/index.php
@@ -4,22 +4,193 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="icon" href="LetterM.ico"/>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="Accueil.css">
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Stalemate&display=swap" rel="stylesheet"> 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <title> Mélange Culturel </title>
+
+  <title> Nom de Page </title>
 </head>
 <body>
-	<?php include('Up.php');  ?>
-	<?php include('Down.html');  ?>
+  <?php  include('Up.html');  ?>
+
+  <forme class="menu">
+
+  <ul class="list-unstyled">
+
+
+
+
+<!-- French-->
+  
+  <section class="francais section" id="french">
+   
+  <div class="plat">
+     <div class="origine" >Des entrées Françaises: </div>
+     <li class="media">
+       <img class="mr-3" src="..." alt="Generic placeholder image">
+       <div class="media-body">
+         <h3 class="mt-0 mb-1 name">French plate</h3>
+           <div class="discription" >
+             Ajaa is made out of blab bllalal lsldasld lqsld lqsdl qlsdl l ls dllsqdnusqdf sdufusdofkdsmflsmdofok  msodf ksdfo.""
+           </div>
+
+           <div class="price">
+           <span class=" titles"> Prix </span> : 19$
+           </div> 
+
+           <div class="stock">
+           <span class=" titles"> Stock </span> : <!-- <div if ($stock>=1) { class="availebel"> availbel </div> }   else { class="unavailebel"> unavailbel </div> -->
+           </div> 
+
+
+       </div>
+     </li>
+  </div> 
+
+  </section>
+
+
+
+
+<!-- Syrian-->
+
+<section class="Syrian section" id="syrian">
+
+<div class="plat"> 
+   <div class="origine" >Des plats Syriens: </div>
+     
+   <li class="media my-4">
+       <img class=" myimg" src="img/syfood.jpg" alt="Generic placeholder image">
+       <div class="media-body">
+         <h3 class="mt-0 mb-1 name">Syrian plate</h3>
+           <div class="discription" >
+           <span class=" titles"> Desciption : </span>Ajaa is made out of blab bllalal lsldasld lqsld lqsdl qlsdl l ls dllsqdnusqdf sdufusdofkdsmflsmdofok  msodf ksdfo.
+           </div>
+           
+           <div class="price">
+            <span class=" titles"> Prix : </span> 19$
+           </div> 
+
+           <div class="stock">
+           <span class=" titles"> Stock : </span>  <!-- <div if ($stock>=1) { class="availebel"> availbel </div> }   else { class="unavailebel"> unavailbel </div> -->
+           </div> 
+           <input type="number" name="platSy1" min="0" step="1" class="form-control quantity"
+          placeholder="Quantité">
+        
+        </div>
+          
+     </li>
+
+   <li class="media my-4">
+       <img class=" myimg" src="img/syfood.jpg" alt="Generic placeholder image">
+       <div class="media-body">
+         <h3 class="mt-0 mb-1 name">Syrian plate</h3>
+           <div class="discription" >
+           <span class=" titles"> Desciption : </span>Ajaa is made out of blab bllalal lsldasld lqsld lqsdl qlsdl l ls dllsqdnusqdf sdufusdofkdsmflsmdofok  msodf ksdfo.
+           </div>
+           
+           <div class="price">
+            <span class=" titles"> Prix : </span> 19$
+           </div> 
+
+           <div class="stock">
+           <span class=" titles"> Stock : </span>  <!-- <div if ($stock>=1) { class="availebel"> availbel </div> }   else { class="unavailebel"> unavailbel </div> -->
+           </div> 
+           <input type="number" name="platSy1" min="0" step="1" class="form-control quantity"
+          placeholder="Quantité">
+       
+        </div>
+          
+     </li>
+   
+   <li class="media my-4">
+       <img class=" myimg" src="img/syfood.jpg" alt="Generic placeholder image">
+       <div class="media-body">
+         <h3 class="mt-0 mb-1 name">Syrian plate</h3>
+           <div class="discription" >
+           <span class=" titles"> Desciption : </span>Ajaa is made out of blab bllalal lsldasld lqsld lqsdl qlsdl l ls dllsqdnusqdf sdufusdofkdsmflsmdofok  msodf ksdfo.
+           </div>
+           
+           <div class="price">
+            <span class=" titles"> Prix : </span> 19$
+           </div> 
+
+           <div class="stock">
+           <span class=" titles"> Stock : </span>  <!-- <div if ($stock>=1) { class="availebel"> availbel </div> }   else { class="unavailebel"> unavailbel </div> -->
+           </div> 
+           <input type="number" name="platSy1" min="0" step="1" class="form-control quantity"
+          placeholder="Quantité">
+          <button class="buttonCom" type="submit"> Commande !</button>
+        </div>
+          
+     </li>
+   </div> 
+
+</section>
+
+
+
+
+<!-- Tunisian-->
+
+  <section class="tunisian section" id="tunisian">
+
+  <div class="plat">  
+     <div class="origine" >  Des desserts Tunisians:  </div>
+      <li class="media">
+        <img class="mr-3" src="..." alt="Generic placeholder image">
+        <div class="media-body">
+          <h3 class="mt-0 mb-1 name">Tunisian desserts</h3>
+             <div class="discription" >
+               Ajaa is made out of blab bllalal lsldasld lqsld lqsdl qlsdl l ls dllsqdnusqdf sdufusdofkdsmflsmdofok  msodf ksdfo.
+             </div>
+             
+           <div class="price">
+           <span class=" titles"> Prix </span> : 19$
+           </div> 
+
+           <div class="stock">
+           <span class=" titles"> Stock </span>: <!-- <div if ($stock>=1) { class="availebel"> availbel </div> }   else { class="unavailebel"> unavailbel </div> -->
+           </div> 
+
+        </div>
+       </li>
+  </div> 
+
+  </section>
+
+
+
+</ul>
+
+
+  </forme>
+
+<section class ="avis" id="avis">
+   <div class="container">
+       <ul class ="ul-avis">
+          <li class="li-avis">
+            <img class="img-portrait" src="img/satea.jpg" alt="Portrait">
+            <blockquote>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, dignissimos quibusdam! Vitae cumque quidem commodi! Ratione fugit soluta aliquam assumenda, corrupti reprehenderit quaerat cum reiciendis. Optio illum consequatur quasi eius. "</blockquote> 
+            <cite> - Satea 
+          </li>
+
+          <li class="li-avis">
+            <img class="img-portrait" src="img/maryam.jpg" alt="Portrait">
+            <blockquote>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, dignissimos quibusdam! Vitae cumque quidem commodi! Ratione fugit soluta aliquam assumenda, corrupti reprehenderit quaerat cum reiciendis. Optio illum consequatur quasi eius. "</blockquote> 
+            <cite> - Maryam
+          </li>
+
+          <li class="li-avis ">
+            <img class="img-portrait" src="img/alexis.gif" alt="Portrait">
+            <blockquote>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, dignissimos quibusdam! Vitae cumque quidem commodi! Ratione fugit soluta aliquam assumenda, corrupti reprehenderit quaerat cum reiciendis. Optio illum consequatur quasi eius. "</blockquote> 
+            <cite> - Alexis
+          </li>
+       </ul>
+   </div>
+  
+</section>
+
+<?php  include('Down.html');  ?>
 </body>
 </html>
